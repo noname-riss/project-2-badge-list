750ab36c0677475f9e4970d3f6e3693df603ba57
rasaighul <afc5865@psu.edu>
2023-04-04 12:06:21 -0400

    search

--- src/search-bar.js
+++ src/search-bar.js
@@ -2,36 +2,58 @@ import { LitElement, html, css } from 'lit';
 
 class SearchBar extends LitElement {
   static properties = {
-    badgeNumber: { 
-      type: String 
-    },
-    backgroundColor: {
-        type: String
-    },
+  
   }
 
+ 
+
   static styles = css`
-    
-    .background {
-
-      background-color: #e9ecf3
-
-    }
-    
+   
+   .form-control {
+    transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    -ms-border-radius: 4px;
+    -o-border-radius: 4px;
+    border-radius: 4px;
+    display: block;
+    width: 50%;
+    height: 34px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.428571429;
+    color: #555555;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075);
+    -webkit-transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
+    -o-transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
+    transition: border-color ease-in-out 0.15s, box-shadow ease-in-out 0.15s;
+    margin: 5em;
+}
   `;
+  
+  
 
   constructor() {
     super();
-    this.badgeNumber = '0';
-    this.backgroundColor = "#e9ecf6";
+  
+
   }
+  
 
   render() {
     return html`
-      <div class='background'>
-      Badges (${this.badgeNumber})
-      
-      </div>
+
+
+    <input class="form-control" type="search" id="site-search" name="q" placeholder = "Search Content, Topics, and People">
+
+  
+
+
     `;
   }
 }
